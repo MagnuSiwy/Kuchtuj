70a99b44650b98974efb729218b097a81a669863
MagnuSiwy <pankacper890@gmail.com>
2024-06-08 16:38:32 +0200

    working create account

--- app/src/main/java/com/example/kuchtuj_v2/CreateAccountActivity.kt
+++ app/src/main/java/com/example/kuchtuj_v2/CreateAccountActivity.kt
@@ -1,4 +1,11 @@
 package com.example.kuchtuj_v2
 
-class CreateAccountActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateAccountActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
 }
